9d86e9daeb9013599913d21b6b1f842625fcf465
Sarper Bilgay Gülfırat <47990987+Sarperbg@users.noreply.github.com>
2022-06-28 18:44:40 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// https://randomuser.me/api/?results=5
+import Header from './components/Header'
+import UserList from './components/UserList'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <UserList />
+    </>
+  )
+}
+
+export default App
